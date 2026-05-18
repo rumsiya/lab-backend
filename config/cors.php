3d5054fd5205fd0d90634721e16b8341c6b917d0
rumsyia <rumsiya@gmail.com>
2026-05-19 01:04:26 +0530

    add project

---
+++ config/cors.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => ['https://star-lab.netlify.app'],
+    // Production example:
+    // 'allowed_origins' => ['https://your-site.netlify.app'],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => false,
+];
